f628f436359f0cfcd15067a8864b19a921407ed7
Aiaru <aiaru.mukhamedyarova@gmail.com>
2023-07-19 18:03:36 +0900

    checking each guess

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
 import words from './words';
 import { useEffect, useState } from 'react';
-import TheBoard from './TheBoard';
+
+const WORD_LEN = 5;
+const LINES = 1;
 
 function App() {
   const [word, setWord] = useState("");
   const [typing, setTyping] = useState("");
+  const [lines, setLine] = useState(new Array(LINES).fill(''));
+  const [index, setIndex] = useState(0);
   useEffect(() => {
-    setWord(words[Math.floor(Math.random()*words.length)]);
+    console.log("INITIAL RENDER");
+    setWord(words[Math.floor(Math.random()*words.length)].toLowerCase());
   }, [])
   const isLetter = (char) => {
     const charCode = char.toLowerCase().charCodeAt(0);
@@ -16,27 +21,58 @@ function App() {
   const onTyping = event => {
     if (event.key === "Backspace") {
       setTyping(typing.slice(0, -1));
-    } else if (event.key === "Enter" && typing.length === 5) {
-      console.log("guess: ",typing);
-    } else if (isLetter(event.key) && typing.length < 5) {
+    } else if (event.key === "Enter" && typing.length === WORD_LEN) {
+      let linesClone = [...lines];
+      linesClone[index] = typing;
+      setLine(linesClone);
+      setIndex(index => index + 1);
+      setTyping('');
+    } else if (isLetter(event.key) && typing.length < WORD_LEN) {
       setTyping(typing + event.key.toLowerCase());
     }
   }
-
-  // useEffect(() => {
-  //   window.addEventListener('keydown', onTyping);
-
-  //   return () => window.removeEventListener('keydown', onPressKey);
-  // }, [typing])
+  if (word == null) return null;
+  
   return (
     <div>
       <h1> TODAY'S RANDOM WORD: {word} </h1>
-      {/* <MyBoard word={word}/> */}
-      <TheBoard word={word}/>
       <div  tabIndex={0} onKeyDown={onTyping}> current guess: {typing}  </div>
+      <div className='board'>
+        {lines.map((line, i) => {
+          console.log(index > i || index === -1);
+          console.log("LINE: ", i, line);
+          return <OneLine 
+            key={i} 
+            line={(i === index ? typing : line).padEnd(WORD_LEN)}
+            toCheck = {index > i || index === -1}
+            word = {word} />;
+        })}
+      </div>
         
     </div>
   );
 }
-
+const OneLine = ({line, toCheck, word}) => {
+return (
+  <div className='line'>
+    { 
+    line.split('').map((char, i) => {
+      let classname = 'tile';
+      console.log(char, " and ", word[i]);
+      if (toCheck) {
+        if (char === word[i]) {
+          classname += ' correct';
+        } else if (word.includes(char)) {
+          classname += ' close';
+        } else {
+          classname += ' incorrect';
+        }
+      }
+      console.log(classname);
+    return <div key={i} className={classname}> {char}</div>;
+  })
+  }
+  </div>
+);
+}
 export default App;

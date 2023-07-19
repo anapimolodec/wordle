34ba14a8df8dcb535f5afbec68df060bbae38fc8
Aiaru <aiaru.mukhamedyarova@gmail.com>
2023-07-19 16:53:31 +0900

    added onKeyDown functionality

--- src/App.js
+++ src/App.js
@@ -5,15 +5,36 @@ import TheBoard from './TheBoard';
 
 function App() {
   const [word, setWord] = useState("");
+  const [typing, setTyping] = useState("");
   useEffect(() => {
     setWord(words[Math.floor(Math.random()*words.length)]);
   }, [])
+  const isLetter = (char) => {
+    const charCode = char.toLowerCase().charCodeAt(0);
+    return (char.length === 1 && charCode >= 97 && charCode <= 122);
+  }
+  const onTyping = event => {
+    if (event.key === "Backspace") {
+      setTyping(typing.slice(0, -1));
+    } else if (event.key === "Enter" && typing.length === 5) {
+      console.log("guess: ",typing);
+    } else if (isLetter(event.key) && typing.length < 5) {
+      setTyping(typing + event.key.toLowerCase());
+    }
+  }
 
+  // useEffect(() => {
+  //   window.addEventListener('keydown', onTyping);
+
+  //   return () => window.removeEventListener('keydown', onPressKey);
+  // }, [typing])
   return (
     <div>
       <h1> TODAY'S RANDOM WORD: {word} </h1>
       {/* <MyBoard word={word}/> */}
-      <TheBoard />
+      <TheBoard word={word}/>
+      <div  tabIndex={0} onKeyDown={onTyping}> current guess: {typing}  </div>
+        
     </div>
   );
 }
